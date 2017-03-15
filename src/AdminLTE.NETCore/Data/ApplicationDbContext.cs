24d8f339855b6249be91ece5433f93c8ff7c6e4f
dariaman <dariaman.siagian@gmail.com>
2017-03-16 01:29:52 +0700

    Initial

---
+++ src/AdminLTE.NETCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AdminLTE.NETCore.Models;
+
+namespace AdminLTE.NETCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //builder.Entity<CardIssuerBankModel>( entity =>
+            //{
+            //    entity.HasOne(d => d.Bank)
+            //});
+            //builder.Entity<bankModel>().HasKey(;
+            //builder.Entity<BankModel>().HasKey(c => c.bank_id);
+            //builder.Ignore<IdentityUserLogin<string>>();
+            //builder.Ignore<IdentityUserRole<string>>();
+            //builder.Ignore<IdentityUserClaim<string>>();
+            //builder.Ignore<IdentityUserToken<string>>();
+            //builder.Ignore<IdentityUser<string>>();
+            //builder.Ignore<ApplicationUser>();
+            //builder.Entity<CardIssuerBankModel>().ToTable("card_issuer_bank");
+            //builder.Entity<BankModel>().ToTable("bank");
+        }
+
+        //public DbSet<CardIssuerBankModel> CardIssuerBankModel { get; set; }
+        //public DbSet<cctypeModel> cctypeModel { get; set; }
+        //public DbSet<BankModel> BankModel { get; set; }
+    }
+}
---
+++ src/AdminLTE.NETCore/Startup.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using AdminLTE.NETCore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.ResponseCompression;
+using System.IO.Compression;
+using AutoMapper;
+using AdminLTE.NETCore.Data;
+using AdminLTE.NETCore.Services;
+using MySQL.Data.Entity.Extensions;
+using AdminLTE.NETCore.Models;
+
+namespace AdminLTE.NETCore
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            //if(env.IsDevelopment())
+            //{
+            //    //builder.AddUserSecrets<Startup>();
+            //    builder.AddUserSecrets();
+            //    //builder.AddApplicationInsightsSettings(developerMode: true);
+            //}
+
+            builder.AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //Add Memory Cache
+            //https://docs.microsoft.com/en-us/aspnet/core/performance/caching/memory
+            services.AddMemoryCache();
+
+            services.Configure<GzipCompressionProviderOptions>
+                (options => options.Level = CompressionLevel.Fastest);
+                services.AddResponseCompression(options =>
+                {
+                    options.Providers.Add<GzipCompressionProvider>();
+                });
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("jbsDB")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(
+                options => {
+                    // configure identity options
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    //options.Password.RequiredLength = 7;
+                    options.Cookies.ApplicationCookie.AutomaticAuthenticate = true;
+                    options.Cookies.ApplicationCookie.AutomaticChallenge = true;
+                    //options.Cookies.ApplicationCookie.LoginPath = "/Account/Login";
+
+                    // User settings
+                    options.User.RequireUniqueEmail = true;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+
+            // Add framework services.
+            services.AddMvcCore();
+            services.AddMvc();
+            services.AddAutoMapper();
+
+            //Add the ControllerInformationRepository
+            services.AddSingleton<IControllerInformationRepository, ControllerInformationRepository>();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error"); //Need to provide a replacement.
+            }
+
+            app.UseStaticFiles();
+            app.UseIdentity();
+            app.UseResponseCompression();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Dashboard}/{action=Dashboardv1}/{id?}");
+            });
+        }
+    }
+}
